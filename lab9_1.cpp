97d3c243bfa0fa2ea21db2915f4422e84e2e6e0f
STRSPH <nahmpiyanan@gmail.com>
2022-12-23 15:18:03 +0700

    lab9_1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main()
+/*int main()
 {
   char rank;
   cout << "Input your rank: ";
@@ -14,4 +14,21 @@ int main()
     case 'D': cout << "You have received very KAK items.\n";
   }
   return 0;
+}*/
+
+int main(){
+  char rank ;
+  cout << "Input your rank: ";
+  cin >> rank;
+  if (rank=='S'){
+    cout << "You have received Super Ultra Rare Unit!!!\n";
+  }if(rank=='S' || rank=='A'){
+    cout << "You have received 5 gems.\n";
+  }if(rank=='S' || rank=='A' || rank=='B'){
+    cout << "You have received 1 gems.\n";
+  }if(rank=='S' || rank=='A' || rank=='B' || rank=='C'){
+    cout << "You have received 2000 coins.\n";
+  }if(rank=='S' || rank=='A' || rank=='B' || rank=='C' || rank=='D'){
+    cout << "You have received very KAK items.\n";
+  }
 }
